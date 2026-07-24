5e3c8dc8e729718076e4518c7b61c6a25a806112
yugam-tyagi <tyagiyugam@gmail.com>
2026-07-24 18:34:18 +0530

    Added RateLimitPolicy model

---
+++ src/main/java/com/yugam/ratelimiter/enums/AlgorithmType.java
@@ -0,0 +1,8 @@
+package com.yugam.ratelimiter.enums;
+
+public enum AlgorithmType {
+    FIXED_WINDOW,
+    SLIDING_WINDOW,
+    TOKEN_BUCKET,
+    LEAKY_BUCKET
+}
---
+++ src/main/java/com/yugam/ratelimiter/model/RateLimitPolicy.java
@@ -0,0 +1,17 @@
+package com.yugam.ratelimiter.model;
+
+import com.yugam.ratelimiter.enums.AlgorithmType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Duration;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RateLimitPolicy {
+    private final int maxRequests;
+    private final Duration windowDuration;
+    private final AlgorithmType algorithm;
+}
